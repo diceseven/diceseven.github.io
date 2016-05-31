f5a3ba1521cd63dbbb9c238aa36e5015939977d1
diceseven <warezeuro@hotmail.com>
2016-05-31 19:56:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 1211,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'Flip the Bitcoin',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LdfUCETAAAAAFAMU4LUOVMcRLzi6SoTHEbwp4Ve',  // <----- EDIT ME!
-  redirect_uri: 'https://diceseven.github.io',
+  redirect_uri: 'https://flipthebitcoin.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
